a7564c1d8b38909fe3cbb4967a79034407377088
Cicero Oliverio <cicero_oliverio@hotmail.com>
2018-10-14 15:05:38 -0300

    Iniciando Capitulo 7

--- js/form.js
+++ js/form.js
@@ -9,10 +9,10 @@ botaoAdicionar.addEventListener("click", function(event) {
     var pacienteTr = montaTr(paciente);
 
     var erros = validaPaciente(paciente);
+    console.log(erros);
 
     if(erros.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -22,7 +22,20 @@ botaoAdicionar.addEventListener("click", function(event) {
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+
+}
 
 function obtemPacienteDoFormulario(form) {
 
@@ -63,12 +76,28 @@ function validaPaciente(paciente){
 
     var erros = [];
 
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
     if(!validaPeso(paciente.peso)){
         erros.push("Peso é inválido");
     }
 
     if(!validaAltura(paciente.altura)){
         erros.push("Altura é inválida");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco");
     }
 
     return erros;
