45a353232904265b1876d5138abf2f19d9381971
Cicero Olivério <cicero_oliverio@hotmail.com>
2018-10-16 20:18:05 -0300

    Projeto Finalizado

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -5,16 +5,24 @@ botaoAdicionar.addEventListener("click", function(){
 
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/paciente");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
+        var erroAjax = document.querySelector("#erro-ajax");
+        if(xhr.status == 200){
+            erroAjax.classList.remove("invisivel");
+            var resposta = xhr.responseText;
 
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            var pacientes = JSON.parse(resposta);
+    
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
     });
 
     xhr.send();
